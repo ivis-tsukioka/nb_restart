02d5cb765062f6ca4ab2e67ccdfc278f0e77641b
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-09-07 14:41:57 +0900

    fix ex_nb

--- nb_restart/nbextension/main.js
+++ nb_restart/nbextension/main.js
@@ -3,9 +3,26 @@ define([
 ], function(Jupyter) {
     function restartKernelIfRequested() {
         var urlParams = new URLSearchParams(window.location.search);
-        console.log('restart is ')
-        console.log(urlParams.get('restart'))
+        is_restart = urlParams.get('restart')
+        console.log(`restart is ${is_restart}`)
         if (urlParams.get('restart') === 'true') {
+            console.log(`[NB DEBUDG] Restart notebook`)
+            //Jupyter.notebook.kernel.restart();
+            console.log(`[NB DEBUDG] Release of freeze`)
+            Jupyter.notebook.get_cells().forEach(function(cell) {
+                if (cell.metadata.hasOwnProperty('run_through_control') &&
+                    typeof cell.metadata.run_through_control === 'object' &&
+                    cell.metadata.run_through_control !== null &&
+                    cell.metadata.run_through_control.hasOwnProperty('frozen')) {
+                    // 値を変更
+                    cell.metadata.run_through_control.frozen = false;
+                }
+            });
+            console.log(`[NB DEBUDG] Clear Output`)
+            Jupyter.notebook.clear_all_output();
+            console.log(`[NB DEBUDG] Checkpoint saving`)
+            IPython.notebook.save_checkpoint();
+            console.log(`[NB DEBUDG] kernel restart`)
             Jupyter.notebook.kernel.restart();
         }
     }
